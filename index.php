1165de8edff33a5e7b7e0f8887a9ca083ebb5433
CIT173F-37@M307.UC-BCF.EDU.PH <CIT173F-37@M307.UC-BCF.EDU.PH>
2022-10-20 10:58:12 +0800

    changes in the php

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
     <form method="post" action="registration.html">
         <div class="input-group">
             <label>First Name</label>
-            <input type="text" name="username" value="">
+            <input type="text" name="username" value="" placeholder ="Lebron Raymone">
         </div>
         <div class="input-group">
             <label>Last Name</label>
-            <input type="text" name="email" value="">
+            <input type="text" name="email" value="" placeholder ="James">
         </div>
         <div>
         <INPUT TYPE="Radio" Name="Gender" Value="Male">Male
@@ -25,11 +25,15 @@
             </div>
         <div class="input-group">
             <label>Email</label>
-            <input type="email" name="email" value="">
+            <input type="email" name="email" value="" placeholder ="lebronjames@gmail.com">
+        </div>
+		 <div class="input-group">
+            <label>Cellphone Number</label>
+            <input type="text" name="cell number" placeholder ="09001002921" maxlength="11" oninput ="this.value = this.value.replace(/[^0-9-]/g,'').replace(/(\..*)\./g,'$1');">
         </div>
         <div class="input-group">
             <label>ID Number</label>
-            <input type="text" name="id number" maxlength="11" oninput ="this.value = this.value.replace(/[^0-9-]/g,'').replace(/(\..*)\./g,'$1');">
+            <input type="text" name="id number"placeholder ="19-2191-639" maxlength="11" oninput ="this.value = this.value.replace(/[^0-9-]/g,'').replace(/(\..*)\./g,'$1');">
         </div>
         <div>
         <label for="birthday">Birthday:</label>
@@ -39,14 +43,19 @@
             <label>Address</label>
             <input type="text" name="Address">
         </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="password_1">
-        </div>
-        <div class="input-group">
-            <label>Confirm Password</label>
-            <input type="password" name="password_2">
-        </div>
+     <form action="/action_page.php">
+  <label for="cars">Choose a Course:</label>
+  <select name="courses" id="course">
+    <option value="BSIT">Bachelor of Science In Information Technology</option>
+    <option value="BSCS">Bachelor of Science In Computer Science</option>
+    <option value="BAPS">Bachelor of Arts in Political Science</option>
+    <option value="BSCPE">Bachelor of Science in Computer Engineering</option>
+	<option value="BSAcc">Bachelor of Science in Accountancy</option>
+	<option value="BSArchi">Bachelor of Science in Architecture</option>
+  </select>
+  <br><br>
+  <input type="submit" value="Submit">
+	</form>
         <div class="input-group">
             <button type="submit" class="btn" name="reg_user">Register</button>
         </div>
